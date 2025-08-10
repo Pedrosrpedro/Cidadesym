2d4307bbc830faaf1f170ea9fa2518694def11aa
Pedrosrpedro <forterobo@gmail.com>
2025-08-10 19:56:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 // main.js
 
+// Este é o único ponto de entrada. Garante que todo o HTML foi carregado.
 document.addEventListener('DOMContentLoaded', () => {
-    DebugConsole.init();
+    // Inicializa os sistemas que cuidam da Interface.
+    // Eles apenas configuram os botões, não iniciam o jogo.
     Menu.init();
-    UI.init(); // Inicializa a UI do jogo
+    UI.init();
+
+    // A lógica do jogo 3D (Game.init) só será chamada
+    // quando o botão "Novo Jogo" for clicado dentro do Menu.
+    console.log("CidadeSym pronto.");
 });
